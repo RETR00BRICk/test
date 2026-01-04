bae832550076cf82da4ed1928b5e97f568e4beb5
RETR00BRICk <retrobrick@tutamail.com>
2026-01-05 01:04:07 +0200

    homeworkTASK1

---
+++ IT STEP/P511/hw1/task1.py
@@ -0,0 +1,8 @@
+first_num = float(input("Введіть перше число: "))
+second_num = float(input("Введіть друге число: "))
+third_num = float(input("Введіть третє число: "))
+
+product = first_num*second_num*third_num
+print(f"Добуток числел {first_num}, {second_num}, {third_num} дорівнює {product}") 
+summ = first_num+second_num+third_num
+print(f"Сума числел {first_num}, {second_num}, {third_num} дорівнює {summ}") 
